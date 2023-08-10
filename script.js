16097286d402a58e528c1a9b700b25cf63de879a
floatingtales <58957425+floatingtales@users.noreply.github.com>
2023-08-10 16:04:41 +0700

    testing whether this works

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 const client = ZAFClient.init();
 client.invoke("resize", {
   width: "100%",
@@ -5,15 +7,36 @@ client.invoke("resize", {
 });
 
 const generateReply = () => {
-  client.invoke("comment.appendText", "test one two three");
+  client.get("ticket.conversation").then(async (data) => {
+    const chat = [];
+
+    for (const conversation of data.ticket.conversation) {
+      chat.push({
+        role: conversation.author.role,
+        content: conversation.message.content,
+      });
+    }
+
+    const reply = await axios.post(
+      "https://empty-cloths-peel.loca.lt/getAIReply",
+      { chat }
+    );
+    client.invoke("comment.appendText", reply[0].message.content);
+  });
 };
 
 client.on("ticket.conversation.changed", () => {
   console.log("conversation changed");
-  client.get("ticket.conversation").then((data) => {
-    console.log("getting data");
-    console.log(data);
-
-    generateReply();
+  client.get("ticket.conversation").then(async (data) => {
+    if (
+      data.ticket.conversation.at(-1).author.role === "agent" ||
+      data.ticket.conversation.at(-1).author.role === "system" ||
+      data.ticket.conversation.at(-1).author.role === "admin"
+    ) {
+      console.log("agent/admin/system messaged last");
+    } else {
+      console.log("client messaged last");
+      generateReply();
+    }
   });
 });
